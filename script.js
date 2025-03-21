e09bae606a2524a3cfaa4c935f4c3ebe8478c678
Deborah Imaji <101178292+DeborahImaji@users.noreply.github.com>
2025-03-21 13:16:51 +0100

    Added the JS code for random colour generation

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 
-function changeBackground() { }
+function changeBackground() {
+    let hex = Math.floor(Math.random() * 16777215).toString(16);
+    let randomColour = `#${hex}`;
+
+    let card = document.querySelector("#card");
+    card.style.backgroundColor = randomColour;
+}
 
 let backgroundButton = document.querySelector("#background-button");
 backgroundButton.addEventListener('click', changeBackground);
